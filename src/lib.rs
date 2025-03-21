734a22355ec410190c558301e842eb26313d2a04
cryptocoal <cryptocoal@outlook.com>
2025-03-20 23:42:39 -0500

    NUID Path for Geyser Plugin Repo

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+use rand::{distributions::Alphanumeric, thread_rng, Rng};
+
+const PRE_LEN: usize = 12;
+
+pub fn next_id() -> String {
+    let mut rng = thread_rng();
+    rng.sample_iter(&Alphanumeric)
+        .take(PRE_LEN)
+        .map(char::from)
+        .collect()
+}
